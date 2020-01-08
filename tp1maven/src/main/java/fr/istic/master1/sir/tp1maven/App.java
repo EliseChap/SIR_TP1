0292eab6aa8d83e1e62fa699ec46ca1a4c35261e
chaponelise@gmail.com <chaponelise@gmail.com>
2020-01-08 16:39:40 +0100

    TP1

---
+++ tp1maven/src/main/java/fr/istic/master1/sir/tp1maven/App.java
@@ -0,0 +1,14 @@
+package fr.istic.master1.sir.tp1maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
